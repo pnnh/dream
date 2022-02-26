dbcf8aa2ef3543eeca672366877952e624587e09
Larry <xspanni@gmail.com>
2022-02-26 07:51:13 +0000

    实现rust调用c++方法，其中c++使用了boost库，并进行了静态链接

--- projects/sfx-server/src/lib.cc
+++ projects/sfx-server/src/lib.cc
@@ -6,9 +6,9 @@
 #include <iostream>
 #include "src/utils/mime.h"
 //#include "src/services/database/postgresql/pq.h"
-//#include "src/utils/datetime.h"
-//#include <boost/property_tree/ptree.hpp>
-//#include <boost/property_tree/xml_parser.hpp>
+#include "src/utils/datetime.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 //
 //std::string HandleSitemap() {
 //    boost::property_tree::ptree pt;
@@ -37,6 +37,24 @@ int list_file(int input) {
     printf("input2: %d\n", input);
     std::string hh = "hhh";
     std::cout << "hh: " << hh << std::endl;
+
+    boost::property_tree::ptree pt;
+
+    pt.put("urlset.<xmlattr>.xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+    boost::property_tree::ptree homeNode;
+    homeNode.put("url.loc", "https://sfx.xyz/");
+    pt.add_child("urlset.url", homeNode.get_child("url"));
+
+    std::string articlesList[] = {"a", "b", "c"};
+    for (const auto &article: articlesList) {
+        std::cout << "article is " << article << std::endl;
+        boost::property_tree::ptree urlNode;
+        urlNode.put("url.loc", "https://sfx.xyz/article/read/" + article);
+        pt.add_child("urlset.url", urlNode.get_child("url"));
+    }
+    std::ostringstream oss;
+    boost::property_tree::write_xml(oss, pt);
+    std::cout << "xml is " << oss.str() << std::endl;
 
     return input * 2;
 }
