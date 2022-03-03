7aec776e51ac6fcb92a018fe2e9daba289c5a365
larry <xspanni@gmail.com>
2022-03-03 15:01:04 +0800

    增加根据平台呈现视图的示例

--- projects/sfx-client/lib/main.dart
+++ projects/sfx-client/lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
 
 import 'application/application.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+
 
 void main() {
-  runApp(SFXApplication());
+
+  var platformName = '';
+  if (kIsWeb) {
+    platformName = "Web";
+  } else {
+    if (Platform.isAndroid) {
+      platformName = "Android";
+    } else if (Platform.isIOS) {
+      platformName = "IOS";
+    } else if (Platform.isFuchsia) {
+      platformName = "Fuchsia";
+    } else if (Platform.isLinux) {
+      platformName = "Linux";
+    } else if (Platform.isMacOS) {
+      platformName = "MacOS";
+    } else if (Platform.isWindows) {
+      platformName = "Windows";
+    }
+  }
+  print("platformName :- "+platformName.toString());
+
+  if (kIsWeb) {
+    runApp(SFXApplication());
+  } else {
+    runApp(
+      const Center(
+        child: Text(
+          'Hello, world!',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+
 }
