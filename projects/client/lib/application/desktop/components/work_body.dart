9a1604b1824ccbea4b802b366914e8ade9689857
larry <xspanni@gmail.com>
2022-03-29 15:12:55 +0800

    实现点击空白部分关闭日期选择控件

--- projects/client/lib/application/desktop/components/work_body.dart
+++ projects/client/lib/application/desktop/components/work_body.dart
@@ -1,3 +1,4 @@
+import 'package:dream/application/desktop/provider/home.dart';
 import 'package:dream/application/desktop/provider/todo.dart';
 import 'package:dream/services/models/task.dart';
 import 'package:dream/widgets/datepicker.dart';
@@ -21,101 +22,101 @@ class _WorkBodyWidget extends State<WorkBodyWidget> {
   final Color selectedColor = const Color.fromRGBO(0, 119, 212, 100);
   final Color defaultColor = const Color.fromRGBO(146, 148, 152, 100);
   final bodyController = TextEditingController();
-  bool showDatePicker = false;
 
   @override
   Widget build(BuildContext context) {
-    final todoListModel = Provider.of<TodoProvider>(context);
+    final todoProvider = Provider.of<TodoProvider>(context);
+    final homeProvider = Provider.of<HomeProvider>(context);
     bodyController.text = widget.task.body;
     return Padding(
-        padding: const EdgeInsets.all(16),
-        child:
-
-          Stack(
-            children: [
+      padding: const EdgeInsets.all(16),
+      child:
+      Stack(
+        children: [
           Column(children: [
-              SizedBox(
-                  height: 32,
-                  child: Row(children: [
-                    SizedBox(
-                        height: 20,
-                        width: 20,
-                        child: SvgPicture.asset(
-                          "images/svg/calendar_2.svg",
-                          color: const Color.fromRGBO(153, 153, 153, 100),
+            SizedBox(
+                height: 32,
+                child: Row(children: [
+                  SizedBox(
+                      height: 20,
+                      width: 20,
+                      child: SvgPicture.asset(
+                        "images/svg/calendar_2.svg",
+                        color: const Color.fromRGBO(153, 153, 153, 100),
+                      )),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    child: Text("设置提醒",
+                        style: TextStyle(
+                          color: Colors.black,
                         )),
-                    const SizedBox(width: 8),
-                    TextButton(
-                      child: Text("设置提醒",
-                          style: TextStyle(
-                            color: Colors.black,
-                          )),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.white),
-                        overlayColor: MaterialStateProperty.all(Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          showDatePicker = !showDatePicker;
-                        });
-                      },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      overlayColor: MaterialStateProperty.all(Colors.white),
                     ),
-                  ])),
-              TextField(
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.all(4),
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: Colors.white,
-                  hoverColor: Colors.white,
-                  hintText: "任务标题",
-                ),
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                ),
-                controller: widget.controller,
-                onChanged: (text) {
-                  todoListModel.putItem(widget.task.key, text, widget.task.body);
-                },
-              ),
-              const SizedBox(height: 24),
-              Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.multiline,
-                    decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(4),
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.white,
-                      hoverColor: Colors.white,
-                      hintText: "任务正文",
-                    ),
-                    controller: bodyController,
-                    onChanged: (text) {
-                      print("WorkBodyWidget body update $text");
-                      todoListModel.putItem(widget.task.key, widget.task.title, text);
+                    onPressed: () {
+                      setState(() {
+                        homeProvider.switchDatePicker();
+                      });
                     },
-                  ))
-      ]),
-            if (showDatePicker)
-              Positioned(
-                child: Container(
-                     height: 200, width: 200,
-                  decoration: BoxDecoration(color: Colors.white,
-                      border: Border.all(
-                          color: Color.fromRGBO(229, 229, 229, 100),
-                          width: 1, ),
-                    borderRadius: BorderRadius.all(const Radius.circular(4)),
                   ),
-                  child: MyDatePickerApp(),
+                ])),
+            TextField(
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(4),
+                border: InputBorder.none,
+                filled: true,
+                fillColor: Colors.white,
+                hoverColor: Colors.white,
+                hintText: "任务标题",
+              ),
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+              controller: widget.controller,
+              onChanged: (text) {
+                todoProvider.putItem(widget.task.key, text, widget.task.body);
+              },
+            ),
+            const SizedBox(height: 24),
+            Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(4),
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: Colors.white,
+                    hoverColor: Colors.white,
+                    hintText: "任务正文",
+                  ),
+                  controller: bodyController,
+                  onChanged: (text) {
+                    print("WorkBodyWidget body update $text");
+                    todoProvider.putItem(
+                        widget.task.key, widget.task.title, text);
+                  },
+                ))
+          ]),
+          if (homeProvider.showDatePicker)
+            Positioned(
+              child: Container(
+                height: 200, width: 200,
+                decoration: BoxDecoration(color: Colors.white,
+                  border: Border.all(
+                    color: Color.fromRGBO(229, 229, 229, 100),
+                    width: 1,),
+                  borderRadius: BorderRadius.all(const Radius.circular(4)),
                 ),
-                left: 00,
-                top: 40,
-              )
-            ],
-          ),
-        );
+                child: MyDatePickerApp(),
+              ),
+              left: 00,
+              top: 40,
+            )
+        ],
+      ),
+    );
   }
 }
--- projects/client/lib/application/desktop/pages/home.dart
+++ projects/client/lib/application/desktop/pages/home.dart
@@ -13,23 +13,28 @@ class HomePageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final homeProvider = Provider.of<HomeProvider>(context);
     return Expanded(
-        child: Row(children: [
-      Container(
-          width: 56,
-          padding: const EdgeInsets.only(top: 8),
-          color: const Color.fromRGBO(242, 246, 255, 100),
-          child: const WorkGroupWidget()),
-      if (homeProvider.showFilter)
-        Container(
-            width: 240,
-            decoration: const BoxDecoration(
-                border: Border(
-                    right: BorderSide(
-              color: Color.fromRGBO(229, 229, 229, 100),
-              width: 1,
-            ))),
-            child: const FilterGroupWidget()),
-      const TodoListWidget(),
-    ]));
+        child: GestureDetector(
+            onTapDown: (detail) {
+              print("out click");
+              homeProvider.closeDatePicker();
+            },
+            child: Row(children: [
+              Container(
+                  width: 56,
+                  padding: const EdgeInsets.only(top: 8),
+                  color: const Color.fromRGBO(242, 246, 255, 100),
+                  child: const WorkGroupWidget()),
+              if (homeProvider.showFilter)
+                Container(
+                    width: 240,
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            right: BorderSide(
+                      color: Color.fromRGBO(229, 229, 229, 100),
+                      width: 1,
+                    ))),
+                    child: const FilterGroupWidget()),
+              const TodoListWidget(),
+            ])));
   }
 }
